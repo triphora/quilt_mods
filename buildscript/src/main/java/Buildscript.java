d6bc33a3d075713222acb5a0d498fc69adf539e6
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-05-25 18:47:01 -0400

    Add configuration and migrate to Quilt
    resolves #1

--- buildscript/src/main/java/Buildscript.java
+++ buildscript/src/main/java/Buildscript.java
@@ -1,64 +1,95 @@
 import io.github.coolcrabs.brachyura.decompiler.BrachyuraDecompiler;
 import io.github.coolcrabs.brachyura.decompiler.fernflower.FernflowerDecompiler;
-import io.github.coolcrabs.brachyura.fabric.FabricLoader;
-import io.github.coolcrabs.brachyura.fabric.FabricMaven;
-import io.github.coolcrabs.brachyura.fabric.FabricProject;
-import io.github.coolcrabs.brachyura.fabric.Yarn;
+import io.github.coolcrabs.brachyura.minecraft.Minecraft;
+import io.github.coolcrabs.brachyura.minecraft.VersionMeta;
+import io.github.coolcrabs.brachyura.fabric.FabricContext.*;
+import io.github.coolcrabs.brachyura.fabric.*;
+import io.github.coolcrabs.brachyura.quilt.*;
 import io.github.coolcrabs.brachyura.maven.Maven;
 import io.github.coolcrabs.brachyura.maven.MavenId;
 import net.fabricmc.mappingio.tree.MappingTree;
 import org.jetbrains.annotations.Nullable;
 
-import static io.github.coolcrabs.brachyura.fabric.FabricProject.ModDependencyFlag.*;
+import static io.github.coolcrabs.brachyura.fabric.FabricContext.ModDependencyFlag.*;
 
 @SuppressWarnings("unused")
-public class Buildscript extends FabricProject {
-  private static final String QUILT_MAVEN = "https://maven.quiltmc.org/repository/release";
+public class Buildscript extends SimpleQuiltProject {
   private static final String MODRINTH_MAVEN = "https://api.modrinth.com/maven";
   private static final String TERRAFORMERS_MAVEN = "https://maven.terraformersmc.com/releases";
 
+  private static final String MC_VERSION = "1.18.2";
+  private static final String QSL_VERSION = "1.1.0-beta.12";
+  private static final String QFAPI_VERSION = "1.0.0-beta.13+0.51.1";
+
   @Override
-  public String getMcVersion() {
-    return "1.18.2";
+  public VersionMeta createMcVersion() {
+    return Minecraft.getVersion(MC_VERSION);
   }
 
   @Override
   public MappingTree createMappings() {
-    return Yarn.ofMaven(FabricMaven.URL, FabricMaven.yarn("1.18.2+build.2")).tree;
+    return QuiltMappings.ofMaven(QuiltMaven.URL, QuiltMaven.quiltMappings(MC_VERSION + "+build.24")).toIntermediary(this.context.get().intermediary.get());
   }
 
   @Override
   public FabricLoader getLoader() {
-    return new FabricLoader(FabricMaven.URL, FabricMaven.loader("0.13.3"));
+    return new FabricLoader(QuiltMaven.URL, QuiltMaven.loader("0.16.1"));
   }
 
   @Override
   public @Nullable BrachyuraDecompiler decompiler() {
-    return new FernflowerDecompiler(Maven.getMavenJarDep(QUILT_MAVEN, new MavenId("org.quiltmc:quiltflower:1.7.0")));
+    return new FernflowerDecompiler(Maven.getMavenJarDep(QuiltMaven.URL, new MavenId("org.quiltmc:quiltflower:1.8.1")));
+  }
+
+  @Override
+  public int getJavaVersion() {
+    return 17;
   }
 
   @Override
   public void getModDependencies(ModDependencyCollector d) {
-    // https://maven.fabricmc.net/net/fabricmc/fabric-api/
-    String[][] fapiModules = new String[][] {
-            {"api-base", "0.4.3+d7c144a8d2"},
-            {"key-binding-api-v1", "1.0.11+54e5b2ecd2"},
-            {"lifecycle-events-v1", "2.0.1+25407454d2"},
+    // https://maven.quiltmc.org/repository/release/org/quiltmc/qsl/
+    String[] qslModules = new String[] {
+            "core:qsl_base",
+            "core:lifecycle_events",
+            "core:networking",
     };
-    String[][] runtimeFapiModules = new String[][] {
-            {"rendering-data-attachment-v1", "0.3.6+d7c144a8d2"},
-            {"rendering-fluids-v1", "2.0.1+54e5b2ecd2"},
-            {"resource-loader-v0", "0.4.17+801ec85b60"},
-            {"screen-api-v1", "1.0.9+d882b915d2"},
+    String[] runtimeQslModules = new String[] {
+            "core:resource_loader",
+            "data:registry_entry_attachments",
+            "data:tags",
+            "gui:screen",
+            "gui:tooltip",
     };
-    for (String[] module : fapiModules) d.addMaven(FabricMaven.URL,
-            new MavenId(FabricMaven.GROUP_ID + ".fabric-api:fabric-" + module[0] + ':' + module[1]), RUNTIME, COMPILE);
-    for (String[] module : runtimeFapiModules) d.addMaven(FabricMaven.URL,
-            new MavenId(FabricMaven.GROUP_ID + ".fabric-api:fabric-" + module[0] + ':' + module[1]), RUNTIME);
+    for (String module : qslModules) d.addMaven(QuiltMaven.URL,
+            new MavenId(QuiltMaven.GROUP_ID + ".qsl." + module + ':' + QSL_VERSION + '+' + MC_VERSION), RUNTIME, COMPILE);
+    for (String module : runtimeQslModules) d.addMaven(QuiltMaven.URL,
+            new MavenId(QuiltMaven.GROUP_ID + ".qsl." + module + ':' + QSL_VERSION + '+' + MC_VERSION), RUNTIME);
 
-    d.addMaven(TERRAFORMERS_MAVEN, new MavenId("com.terraformersmc:modmenu:3.0.1"), RUNTIME);
+    // https://maven.fabricmc.net/net/fabricmc/fabric-api/
+    String[] qfapiModules = new String[] {
+            "api-base",
+            "key-binding-api-v1",
+            "lifecycle-events-v1",
+    };
+    String[] runtimeQfapiModules = new String[] {
+            "renderer-registries-v1",
+            "rendering-v1",
+            "rendering-data-attachment-v1",
+            "rendering-fluids-v1",
+            "resource-loader-v0",
+            "screen-api-v1",
+    };
+    for (String module : qfapiModules) d.addMaven(QuiltMaven.URL,
+            new MavenId(QuiltMaven.GROUP_ID + ".quilted-fabric-api:fabric-" + module + ':' + QFAPI_VERSION + '-' + MC_VERSION), RUNTIME, COMPILE);
+    for (String module : runtimeQfapiModules) d.addMaven(QuiltMaven.URL,
+            new MavenId(QuiltMaven.GROUP_ID + ".quilted-fabric-api:fabric-" + module + ':' + QFAPI_VERSION + '-' + MC_VERSION), RUNTIME);
+
+    jij(d.addMaven(MODRINTH_MAVEN, new MavenId("maven.modrinth:midnightlib:0.4.0"), RUNTIME, COMPILE));
+
+    d.addMaven(TERRAFORMERS_MAVEN, new MavenId("com.terraformersmc:modmenu:3.2.1"), RUNTIME);
     d.addMaven(MODRINTH_MAVEN, new MavenId("maven.modrinth:sodium:mc1.18.2-0.4.1"), RUNTIME);
-    d.addMaven(MODRINTH_MAVEN, new MavenId("maven.modrinth:lithium:mc1.18.2-0.7.9"), RUNTIME);
+    d.addMaven(MODRINTH_MAVEN, new MavenId("maven.modrinth:lithium:mc1.18.2-0.7.10"), RUNTIME);
     d.addMaven(MODRINTH_MAVEN, new MavenId("maven.modrinth:starlight:1.0.2+1.18.2"), RUNTIME);
     d.addMaven(MODRINTH_MAVEN, new MavenId("maven.modrinth:lazydfu:0.1.2"), RUNTIME);
     d.addMaven(Maven.MAVEN_CENTRAL, new MavenId("org.joml:joml:1.10.2"), RUNTIME);
--- src/main/java/coffee/waffle/wakeuptime/WakeUpTime.java
+++ src/main/java/coffee/waffle/wakeuptime/WakeUpTime.java
@@ -1,25 +1,34 @@
 package coffee.waffle.wakeuptime;
 
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.option.KeyBind;
 import net.minecraft.text.TranslatableText;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+import org.quiltmc.qsl.lifecycle.api.client.event.ClientTickEvents;
 
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_V;
 
 public class WakeUpTime implements ClientModInitializer {
   private static final String CATEGORY = "key.categories.wakeUpTime";
   private static final MinecraftClient client = MinecraftClient.getInstance();
+  private boolean pressed = false;
 
   @Override
-  public void onInitializeClient() {
-    KeyBinding sendStatusToActionBar = new KeyBinding("key.wakeUpTime.sendStatus", GLFW_KEY_V, CATEGORY);
+  public void onInitializeClient(ModContainer mod) {
+    MidnightConfig.init(mod.metadata().id(), Config.class);
+
+    KeyBind sendStatusToActionBar = new KeyBind("key.wakeUpTime.sendStatus", GLFW_KEY_V, CATEGORY);
     KeyBindingHelper.registerKeyBinding(sendStatusToActionBar);
 
-    ClientTickEvents.END_CLIENT_TICK.register(e -> {
-      while (sendStatusToActionBar.wasPressed()) sendStatusToActionBar();
+    ClientTickEvents.END.register(e -> {
+      if (sendStatusToActionBar.wasPressed()) {
+        pressed = !pressed;
+        sendStatusToActionBar();
+      }
+      if (Config.persistent && pressed) sendStatusToActionBar();
     });
   }
 
@@ -43,5 +52,9 @@ public class WakeUpTime implements ClientModInitializer {
     final long timeUntilWork = time > 9000 ? 26000 - time : 2000 - time;
 
     return new TranslatableText("key.wakeUpTime.lazyBums", "§a" + status.getString(), "§a" + timeUntilWork / 20);
+  }
+
+  public static class Config extends MidnightConfig {
+    @Entry public static boolean persistent = false;
   }
 }
