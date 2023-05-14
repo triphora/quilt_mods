09db38a5a29eefb7178a56d10347548beea74c8a
Emma Triphora <emma@modrinth.com>
2023-05-14 18:28:54 -0400

    Update things

--- COMMON.md
+++
@@ -1,7 +0,0 @@
----
-
-[![Support us through GitHub Sponsors](https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@2/assets/cozy/donate/ghsponsors-plural_64h.png)](https://github.com/sponsors/triphora)
-
-[![Chat with us on Discord](https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@2/assets/cozy/social/discord-plural_64h.png)](https://discord.gg/EES4w9qbtk)
-
-[![Codeberg](https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@2/assets/minimal/available/codeberg_64h.png)](https://codeberg.org/emmods/quilt_mods) [![GitHub](https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@2/assets/minimal/available/github_64h.png)](https://github.com/emmods/quilt_mods)
--- buildSrc/src/main/java/dev/triphora/gradle/Constants.java
+++ buildSrc/src/main/java/dev/triphora/gradle/Constants.java
@@ -2,9 +2,9 @@ package dev.triphora.gradle;
 
 public class Constants {
 	public static final String DONATE_LINK = "https://github.com/sponsors/triphora";
-	public static final String DISCORD_LINK = "https://discord.gg/EES4w9qbtk";
-	public static final String SOURCE_LINK = "https://codeberg.org/emmods/quilt_mods";
-	public static final String ISSUES_LINK = "https://codeberg.org/emmods/quilt_mods/issues";
+	public static final String DISCORD_LINK = "https://discord.gg/buPbUvw5FD";
+	public static final String SOURCE_LINK = "https://github.com/emmods/quilt_mods";
+	public static final String ISSUES_LINK = "https://github.com/emmods/quilt_mods/issues";
 	public static final String EMAIL = "emma@modrinth.com";
 	public static final String GROUP = "dev.triphora";
 	public static final String INTERMEDIATE_MAPPINGS = "net.fabricmc:intermediary";
